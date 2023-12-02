6464d189071698cbf36242305f4d3213a84e4bdb
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-12-02 23:25:38 +0900

    servlet 설정

---
+++ src/main/java/moon/servlet/basic/HelloServlet.java
@@ -0,0 +1,17 @@
+package moon.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name="helloServelt", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+}
